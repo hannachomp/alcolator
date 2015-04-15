ca2fd570975d943364df39fc9cfa1dd8fd8c3e76
Hanna Xu <hello@hannaxu.com>
2015-04-14 23:20:08 -0700

    Initial Commit

---
+++ Calcohol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcohol
+//
+//  Created by Hanna Xu on 4/14/15.
+//  Copyright (c) 2015 Hanna Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcohol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcohol
+//
+//  Created by Hanna Xu on 4/14/15.
+//  Copyright (c) 2015 Hanna Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
